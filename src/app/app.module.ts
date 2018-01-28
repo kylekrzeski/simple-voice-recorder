f91cd201c4065ea4b515081d6b5b2231a1017cb1
Kyle Krzeski <krzeski@vt.edu>
2018-01-27 21:34:51 -0500

    added basic recording functionality and a list of recent recordings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,21 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { SettingsPage } from '../pages/settings/settings';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { Media } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
+
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
+    SettingsPage,
     HomePage,
     TabsPage
   ],
@@ -27,14 +30,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
+    SettingsPage,
     HomePage,
     TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Media,
+    File
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Media, MediaObject } from '@ionic-native/media';
+import { File } from '@ionic-native/file';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,80 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  //variables
+  recording: boolean = false;
+  filePath: string;
+  fileName: string;
+  audio: MediaObject;
+  audioList: any[] = [];
 
+  constructor(
+    public navCtrl: NavController,
+    private media: Media,
+    private file: File,
+    public platform: Platform
+  ) { }
+
+  //essentially ngInit
+  ionViewWillEnter() {
+    this.getAudioList();
   }
 
+  //starts the audio recording
+  startRecording(): void {
+    this.recording = true;
+
+    //give the audio clip a name
+    const date = new Date();
+    this.fileName = 'recording_' + date.getDate() + date.getMonth() + date.getFullYear()
+      + date.getHours() + date.getMinutes() + date.getSeconds() + '.m4a';
+
+    //different behavior for each OS
+    if (this.platform.is('ios')) {
+      //create the file path
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+    }
+    else if (this.platform.is('android')) {
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+    }
+    //create the file
+    this.audio = this.media.create(this.filePath);
+
+    //start the audio recording (audio is the media object)
+    this.audio.startRecord();
+  }
+
+
+  //stop recording
+  stopRecording(): void {
+    this.audio.stopRecord();
+    let data = { filename: this.fileName };
+    this.audioList.push(data);
+    //put in local storage
+    localStorage.setItem('audiolist', JSON.stringify(this.audioList));
+    this.recording = false;
+    //get the list of new items (can also just push this one to the list)
+    this.getAudioList();
+  }
+
+
+  //gets a list of the audio files that were recorded
+  getAudioList() {
+    if (localStorage.getItem('audioList')) {
+      this.audioList = JSON.parse(localStorage.getItem('audioList'));
+      //console.log(this.audioList);
+    }
+  }
+
+  play(file, idx) {
+    if (this.platform.is('ios')) {
+      this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+    }
+    else if (this.platform.is('android')) {
+      this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+    }
+    this.audio = this.media.create(this.filePath);
+    this.audio.play();
+    this.audio.setVolume(0.8);
+  }
 }
